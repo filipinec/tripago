be580124e3bc9cad8eec5237b005cf0bee327182
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-03-08 21:29:39 +0100

    Add Hide Trips

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import TripList from './components/TripList';
 
+
 function App() {
+  const [showTrip, setShowTrip] = useState(true)
   return (
     <div className="App">
-     <TripList/>
+      <button onClick={()=> setShowTrip(false)}>Hide Trips</button>
+     {showTrip && <TripList/>}
     </div>
   );
 }
--- src/components/TripList.js
+++ src/components/TripList.js
@@ -5,7 +5,7 @@ import useFetch from './hooks/useFetch'
 
 export default function TripList() {
 
-  const [url, setUrl] = useState(['http://localhost:3000/tripsss'])
+  const [url, setUrl] = useState(['http://localhost:3000/trips'])
   const {data: trips, isPending, error} = useFetch(url)
 
 
